6daa21b1d25b342b5daea63ae4087cda7b7291d9
Natalia Casarin <natalia.casarin02@gmail.com>
2024-02-14 23:31:49 -0600

    Added github url repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Natalia Casarin and is{" "}
+        <a href="https://github.com/natcas9/react-weatherapp" target="_blank">
+          open-source code
+        </a>
+      </footer>
+    </div>
+  );
 }
