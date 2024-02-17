0a80a14d6b4f913fa6fa06dc408f1fb014f816e0
Natalia Casarin <natalia.casarin02@gmail.com>
2024-02-17 13:58:24 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Natalia Casarin and is{" "}
-        <a href="https://github.com/natcas9/react-weatherapp" target="_blank">
-          open-source code
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Natalia Casarin and is{" "}
+          <a href="https://github.com/natcas9/react-weatherapp" target="_blank">
+            open-source code
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
